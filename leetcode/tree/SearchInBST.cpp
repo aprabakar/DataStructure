04e92dbbfe2c76496746a0625eb93c07d3a5a56e
prabakar_a <aprabakar476@gmail.com>
2022-01-08 19:08:21 +0530

    SearchInBST

---
+++ leetcode/tree/SearchInBST.cpp
@@ -0,0 +1,34 @@
+//Recursive
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(!root){
+            return NULL;
+        }
+        if(root->val == val){
+            return root;
+        }
+        
+        if(root->val > val){
+            return searchBST(root->left , val);
+        }
+        
+        return searchBST(root->right , val);
+    }
+};
+
+//Iterative
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        while(root){
+            if(root->val == val){
+                return root;
+            }
+            
+            root = (root->val > val) ? root->left : root->right;
+        }
+        
+        return root;
+    }
+};
