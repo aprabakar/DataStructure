b4b17859ebe471020c95987d6267e0c988d205fd
prabakar_a <aprabakar476@gmail.com>
2022-01-23 14:55:49 +0530

    Median_star

---
+++ leetcode/heap/Median_star.cpp
@@ -0,0 +1,21 @@
+// leet -> https://leetcode.com/explore/learn/card/heap/646/practices/4092/class MedianFinder {
+public:
+    priority_queue<int> maxHeap;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    
+    MedianFinder() {
+    }
+    void addNum(int num) {
+        maxHeap.push(num);
+        minHeap.push(maxHeap.top());
+        maxHeap.pop();
+        if (minHeap.size() > maxHeap.size()) {
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
+    }
+    double findMedian() {
+        if (maxHeap.size() > minHeap.size()) return maxHeap.top();
+        return (maxHeap.top() + minHeap.top()) / 2.0;
+    }
+};
