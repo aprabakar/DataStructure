82047973bd475675a3b4806eb173d9ba7c1f2de9
prabakar_a <aprabakar476@gmail.com>
2022-01-25 22:23:48 +0530

    swimWater_star

---
+++ leetcode/heap/swimWater_star.cpp
@@ -0,0 +1,31 @@
+// leet -> https://leetcode.com/problems/swim-in-rising-water/submissions/
+
+class Solution {
+public:
+    int swimInWater(vector<vector<int>>& grid) {
+        int n = grid.size(), ans = max(grid[0][0], grid[n-1][n-1]);
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        vector<vector<int>> visited(n, vector<int>(n, 0));
+        visited[0][0] = 1;
+        vector<int> dir({-1, 0, 1, 0, -1});
+        pq.push({ans, 0, 0});
+        while (!pq.empty()) {
+            auto cur = pq.top();
+            pq.pop();
+            //cout<<"node = "<<cur[0]<<endl;
+            ans = max(ans, cur[0]);
+            for (int i = 0; i < 4; ++i) {
+                int r = cur[1] + dir[i], c = cur[2] + dir[i+1];
+                if (r >= 0 && r < n && c >= 0 && c < n && visited[r][c] == 0) {
+                    if (r == n-1 && c == n-1){
+                       return ans; 
+                    } 
+                    //cout<< "grid[r][c] = "<<grid[r][c]<<endl;
+                    pq.push({grid[r][c], r, c});
+                    visited[r][c] = 1;
+                }
+            }
+        }
+        return 0;
+    }
+};
