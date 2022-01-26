be3d10a48d9188fbca08f9c211a0fbbcb03c4eab
prabakar_a <aprabakar476@gmail.com>
2022-01-26 21:25:11 +0530

    updated

--- leetcode/algos/binarySearch/FindSmallGreaterThanTarget.cpp
+++ leetcode/algos/binarySearch/FindSmallGreaterThanTarget.cpp
@@ -1,8 +1,6 @@
-// leet -> https://leetcode.com/explore/learn/card/binary-search/137/conclusion/977/
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        char ans;
         int left = 0, right =letters.size()-1;
         
         while(left <= right){
@@ -14,15 +12,7 @@ public:
                 left = mid+1;
             }
         }
-        
-        if(left >= letters.size()){
-            ans = letters[0];
-        }else if(letters[left] == target){
-            ans = letters[(left+1) % letters.size()];
-        }else{
-            ans = letters[left];
-        }
-        
-        return ans;
+                
+        return letters[left%letters.size()];
     }
 };
