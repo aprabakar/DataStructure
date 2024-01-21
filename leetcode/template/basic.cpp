7568aba27336095cca376909242a80ec9cc4d72b
prabakar_a <aprabakar476@gmail.com>
2024-01-21 21:37:33 +0530

    Update basic.cpp

--- leetcode/template/basic.cpp
+++ leetcode/template/basic.cpp
@@ -1,1 +1,44 @@
 int val = stoi(tokens[i]);
+
+// Tuples
+stack<tuple<int,int, int>> st;
+vector<int> ans(temperatures.size());
+
+for(int i=0; i < temperatures.size() ; i++){
+    if(st.empty()){
+        st.push(make_tuple(temperatures[i],i,1)); 
+    }else{
+        
+        int count = 0, rot = 0;
+        while(!st.empty() && get<0>(st.top()) < temperatures[i]){
+            count += get<2>(st.top());
+            ans[get<1>(st.top())] = count;
+            ++rot;
+            st.pop();
+        }
+        st.push(make_tuple(temperatures[i],i, max(rot,1)));
+    }
+}
+
+return ans;
+
+class Car{
+public:
+    Car(int pos, int speed){
+        this->pos = pos;
+        this->speed= speed;
+    }
+    int pos;
+    int speed;
+};
+
+// sort based on field
+vector<Car> cars;
+int N = position.size();
+for(int i = 0; i<N; i++){
+    cars.emplace_back(position.at(i), speed.at(i));
+}
+
+sort(cars.begin(), cars.end(), [](const Car& a, const Car& b){
+    return a.pos<b.pos;
+});
