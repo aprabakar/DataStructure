e1f5ccc72c773f524643b4afb7ab887cdc595e5a
prabakar_a <aprabakar476@gmail.com>
2024-01-27 12:24:12 +0530

    Create Graph.cpp

---
+++ leetcode/template/Graph.cpp
@@ -0,0 +1,34 @@
+//Adjacent matrix:
+
+bool isNotValid(int i, int j, int m, int n){
+        return (i <0 || j<0 || i>=m || j>=n);
+    }
+
+    void dfs(vector<vector<char>>& grid, int row, int col, vector<vector<int>>& visited){
+        visited[row][col] = 1;
+        vector<vector<int>> dir = {{0,-1}, {-1,0}, {0,1}, {1,0}};
+
+        for(int i=0; i < dir.size(); i++){
+            int new_row = row+dir[i][0], new_col = col+dir[i][1];
+
+            if(!isNotValid(new_row, new_col, grid.size(), grid[0].size()) &&
+                !visited[new_row][new_col] && grid[new_row][new_col] == '1'){
+                dfs(grid, new_row, new_col, visited);
+            }
+        }
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        vector<vector<int>> visited(grid.size(), vector<int>(grid[0].size(), 0));
+        int ans = 0;
+        for(int i=0; i < grid.size(); i++){
+            for(int j=0; j < grid[i].size(); j++){
+                if(!visited[i][j] && grid[i][j] == '1'){
+                    visited[i][j] = 1;
+                    dfs(grid, i, j, visited);
+                    ++ans;
+                }
+            }
+        }
+
+        return ans;
+    }
