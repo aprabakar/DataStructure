db2b57cc7d800f90af46c036a8c507cc4aaed5a8
prabakar_a <aprabakar476@gmail.com>
2024-02-02 00:07:02 +0530

    Update Graph.cpp

--- leetcode/template/Graph.cpp
+++ leetcode/template/Graph.cpp
@@ -1,4 +1,5 @@
 //Adjacent matrix:
+//DFS
 
 bool isNotValid(int i, int j, int m, int n){
         return (i <0 || j<0 || i>=m || j>=n);
@@ -137,7 +138,106 @@ if(ans.size() == numCourses){
 return {};
 }
 
+//Djikstra's
 
+int minScore(int n, vector<vector<int>>& roads) {
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> minHeap;
+        vector<int> dist(n+1, INT_MAX);
+        vector<vector<pair<int,int>>> graph(n+1);
+
+        for(int i=0; i < roads.size(); i++){
+            graph[roads[i][0]].push_back(make_pair(roads[i][1],roads[i][2]));
+            graph[roads[i][1]].push_back(make_pair(roads[i][0],roads[i][2]));
+        }
+
+        minHeap.push({INT_MAX, 1});
+
+        while(!minHeap.empty()){
+            int currWeight = minHeap.top().first;
+            int currNode = minHeap.top().second;
+            minHeap.pop();
+
+            for(int i=0; i < graph[currNode].size(); i++){
+                int weight = min(currWeight, graph[currNode][i].second);
+
+                if(weight < dist[graph[currNode][i].first]){
+                    dist[graph[currNode][i].first] = weight;
+                    minHeap.push({weight, graph[currNode][i].first});
+                }
+            }
+        }
+
+        return dist[n];
+    }
+
+//Bellman ford
+
+vector<int> bellman_ford(int V, vector<vector<int>>& edges, int S) {
+        vector<int> dist(V, 1e8);
+        dist[S] = 0;
+        for (int i = 0; i < V - 1; i++) {
+                for (auto it : edges) {
+                        int u = it[0];
+                        int v = it[1];
+                        int wt = it[2];
+                        if (dist[u] != 1e8 && dist[u] + wt < dist[v]) {
+                                dist[v] = dist[u] + wt;
+                        }
+                }
+        }
+        // Nth relaxation to check negative cycle
+        for (auto it : edges) {
+                int u = it[0];
+                int v = it[1];
+                int wt = it[2];
+                if (dist[u] != 1e8 && dist[u] + wt < dist[v]) {
+                        return { -1};
+                }
+        }
+
+
+        return dist;
+	}
+
+    // prim's algo
+    int distance(vector<int>a,vector<int>b){
+        return abs(a[1]-b[1]) + abs(a[0]-b[0]);
+    }
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        vector<pair<int,int>>adj[points.size()];
+        for(int i=0;i<points.size()-1;i++){
+            for(int j=i+1;j<points.size();j++){
+                int w = (distance(points[i],points[j]));
+                adj[i].push_back({w,j});
+                adj[j].push_back({w,i});
+            }
+        }
+        map<int,bool>visited;
+        for(int i=0;i<points.size();i++){
+            visited[i]=false;
+        }
+        int minpath=0;
+        priority_queue<pair<int,int> ,vector<pair<int,int>>, greater<pair<int,int>>>pq;
+        for(auto x:adj[0]){ 
+            pq.push(x);
+        }
+        visited[0]=true;
+        while(!pq.empty()){
+            int u=pq.top().second;
+            int w=pq.top().first;
+            pq.pop();
+            if(visited[u]==false){
+                minpath+=w;
+                visited[u]=true;
+                for(auto x:adj[u]){
+                    if(visited[x.second]==false){
+                        pq.push(x);
+                    }
+                }
+            }
+        }
+        return minpath;
+    }
 
 
 //Problems:
@@ -151,4 +251,21 @@ https://leetcode.com/problems/alien-dictionary
 https://leetcode.com/discuss/interview-question/4276157/Shortest-path-in-Directed-Acyclic-Graph/
 https://leetcode.com/problems/word-ladder
 https://leetcode.com/problems/word-ladder-ii/
+https://leetcode.com/problems/minimum-fuel-cost-to-report-to-the-capital/description/
+https://leetcode.com/problems/minimum-score-of-a-path-between-two-cities/
+https://leetcode.com/problems/number-of-closed-islands/description/
+https://leetcode.com/problems/number-of-enclaves/description/
+https://leetcode.com/problems/minimum-number-of-vertices-to-reach-all-nodes/description/
+https://leetcode.com/problems/is-graph-bipartite/description/
+https://leetcode.com/problems/detonate-the-maximum-bombs/description/
+https://leetcode.com/problems/largest-color-value-in-a-directed-graph/description/
+https://leetcode.com/problems/path-with-minimum-effort/description/
+https://leetcode.com/problems/cheapest-flights-within-k-stops/description/
+https://leetcode.com/problems/number-of-ways-to-arrive-at-destination/
+https://leetcode.com/problems/number-of-provinces/description/
 https://leetcode.com/problems/number-of-operations-to-make-network-connected/description/
+https://leetcode.com/problems/min-cost-to-connect-all-points/
+https://leetcode.com/problems/network-delay-time/
+https://leetcode.com/problems/path-with-maximum-probability/description/
+https://leetcode.com/problems/reconstruct-itinerary/description/
+https://leetcode.com/problems/swim-in-rising-water/description/
