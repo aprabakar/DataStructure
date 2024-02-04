0530a2080fd8ebb17c369d1aaa66c1d1d8c1c52b
prabakar_a <aprabakar476@gmail.com>
2024-02-04 20:13:03 +0530

    Update Recursion.cpp

--- leetcode/template/Recursion.cpp
+++ leetcode/template/Recursion.cpp
@@ -47,6 +47,57 @@ vector<vector<int>> subsetsWithDup(vector<int>& nums) {
     return result;        
 }
 
+// over an array
+//https://leetcode.com/problems/palindrome-partitioning/description/
+ void solve(string s, int index, vector<string> v){
+    if(index == s.length()){
+        if(isPalindrome(v)){
+            ans.push_back(v);
+        }
+        return;
+    }
+
+
+    for (int i = index; i < s.length(); i++) {
+        v.push_back(s.substr(index, i - index + 1));
+        solve(s, i + 1, v);
+        v.pop_back();
+    }
+}
+vector<vector<string>> partition(string s) {
+    solve(s, 0, {});
+    return ans;
+}
+
+//permutation
+https://leetcode.com/problems/permutations/
+void backtrack(int index, vector<int>& nums, vector<int>& list){
+    if(index == nums.size()){
+        ans.push_back(list);
+        return;
+    }
+
+    for(int i=index; i < nums.size(); i++){
+        int temp = nums[i];
+        nums[i] = nums[index];
+        nums[index] = temp;
+
+        list.push_back(nums[index]);
+        backtrack(index+1, nums, list);
+        list.pop_back();
+
+        temp = nums[i];
+        nums[i] = nums[index];
+        nums[index] = temp;            
+    }
+
+    return;
+}
+
 
 //Problems:
 https://leetcode.com/problems/subsets-ii/submissions/1165653292/
+https://leetcode.com/problems/combination-sum/description/
+https://leetcode.com/problems/combination-sum-ii/description/
+https://leetcode.com/problems/palindrome-partitioning/description/
+https://leetcode.com/problems/permutations/
